1aa612dc08352dd10c37adf84c99d420b5b7bc8c
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-09-04 13:48:37 -0400

    Initial Commit - mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// Prevents page from refreshing when you click a nav link
+
+// let allLinks = document.querySelectorAll("a");
+
+// allLinks.addEventListener("click", event => {
+//   event.preventDefault();
+// });
+
+// Images - mouseover - change image
+
+let contentImages = document.querySelectorAll(".img-content img");
+
+contentImages[0].addEventListener("mouseover", event => {
+  event.target.setAttribute("src", "/img/fun.jpg");
+});
+
+contentImages[0].addEventListener("mouseout", event => {
+  event.target.setAttribute("src", "/img/adventure.jpg");
+});
+
+contentImages[1].addEventListener("mouseover", event => {
+  event.target.setAttribute("src", "/img/adventure.jpg");
+});
+
+contentImages[1].addEventListener("mouseout", event => {
+  event.target.setAttribute("src", "/img/fun.jpg");
+});
